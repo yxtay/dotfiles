5034f21306bb28e514e082ef51b7838ee8603525
yuxuantay <5795122+yxtay@users.noreply.github.com>
2024-12-30 13:52:11 +0800

    clean up modules

--- chezmoi/private_dot_config/zsh/pre.zsh
+++ chezmoi/private_dot_config/zsh/pre.zsh
@@ -3,27 +3,17 @@
 # -----------------
 
 #
-# History
-#
-
-# Remove older command from the history if a duplicate is to be added.
-setopt HIST_IGNORE_ALL_DUPS
-
-#
 # Input/output
 #
 
 # Set editor default keymap to emacs (`-e`) or vi (`-v`)
-bindkey -v
+# bindkey -v
 
 # Prompt for spelling correction of commands.
 setopt CORRECT
 
 # Customize spelling correction prompt.
 # SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
-
-# Remove path separator from WORDCHARS.
-WORDCHARS=${WORDCHARS//[\/]}
 
 # --------------------
 # Module configuration
