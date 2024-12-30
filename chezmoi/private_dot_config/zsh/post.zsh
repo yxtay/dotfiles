f9cd7403735aeffa0221dfd698553f3270399e91
yuxuantay <5795122+yxtay@users.noreply.github.com>
2024-12-30 15:12:51 +0800

    move post.zsh

--- chezmoi/private_dot_config/zsh/post.zsh
+++ chezmoi/private_dot_config/zsh/post.zsh
@@ -1,1 +1,16 @@
-# post initiation
+# ------------------------------
+# Post-init module configuration
+# ------------------------------
+
+#
+# zsh-history-substring-search
+#
+
+zmodload -F zsh/terminfo +p:terminfo
+# Bind ^[[A/^[[B manually so up/down works both before and after zle-line-init
+for key ('^[[A' '^P' ${terminfo[kcuu1]}) bindkey ${key} history-substring-search-up
+for key ('^[[B' '^N' ${terminfo[kcud1]}) bindkey ${key} history-substring-search-down
+for key ('k') bindkey -M vicmd ${key} history-substring-search-up
+for key ('j') bindkey -M vicmd ${key} history-substring-search-down
+unset key
+# End configuration added by Zim install
