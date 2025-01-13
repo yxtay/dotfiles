2b784fe66e2fd3db141704f2124a3be9a9326034
yxtay <5795122+yxtay@users.noreply.github.com>
2025-01-13 15:10:23 +0800

    improve nix chezmoi apply

--- home/chezmoi.nix
+++ home/chezmoi.nix
@@ -11,9 +11,9 @@
       chezmoi=${pkgs.chezmoi}/bin/chezmoi
       source_path=$("$chezmoi" source-path)
 
-      cmd="$chezmoi apply"
-      [[ $source_path == /Users/yuxuantay/.local/share/chezmoi || $source_path == /nix/store/* ]] && cmd+=" --init --source ${self}"
-      $cmd
+      args="apply"
+      [[ $source_path == */.local/share/chezmoi || $source_path == /nix/store/* ]] && args+=" --init --source ${self}"
+      "$chezmoi" $args
     fi
   '';
 }
