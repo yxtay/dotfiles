ae495487a18b1d293f76a239dc63adfb8779858e
YuXuan Tay <5795122+yxtay@users.noreply.github.com>
2025-02-01 13:22:16 +0800

    ci: modify (#3)

--- modules/flake-parts/treefmt.nix
+++ modules/flake-parts/treefmt.nix
@@ -3,15 +3,17 @@
   imports = [ inputs.treefmt-nix.flakeModule ];
 
   perSystem = _: {
-    treefmt.programs = {
-      deadnix.enable = true;
-      prettier.enable = true;
-      nixfmt.enable = true;
-      shellcheck.enable = true;
-      shfmt.enable = true;
-      statix.enable = true;
-      taplo.enable = true;
-      yamlfmt.enable = true;
+    treefmt = {
+      programs = {
+        deadnix.enable = true;
+        prettier.enable = true;
+        nixfmt.enable = true;
+        shellcheck.enable = true;
+        shfmt.enable = true;
+        statix.enable = true;
+        taplo.enable = true;
+        yamlfmt.enable = true;
+      };
     };
   };
 }
