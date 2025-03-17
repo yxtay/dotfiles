22ee0927ebe4759e45ed431ea63af084ad0e5fab
yxtay <5795122+yxtay@users.noreply.github.com>
2025-03-17 17:40:06 +0800

    Update nix-core.nix

--- modules/darwin/nix-core.nix
+++ modules/darwin/nix-core.nix
@@ -1,25 +1,32 @@
 inputs@{ pkgs, lib, ... }:
 let
-  extra-substituters = [
-    "https://nix-community.cachix.org"
-    "https://nixpkgs-unfree.cachix.org"
-    "https://cachix.cachix.org"
-    "https://devenv.cachix.org"
-    "https://cache.flox.dev"
-    "https://numtide.cachix.org"
-  ];
-  extra-trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-    "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-    "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-  ];
-  trusted-users = [
-    "root"
-    "@admin"
-  ];
+  settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-unfree.cachix.org"
+      "https://cachix.cachix.org"
+      "https://devenv.cachix.org"
+      "https://cache.flox.dev"
+      "https://numtide.cachix.org"
+      "https://cache.flakehub.com"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+    trusted-users = [
+      "root"
+      "@admin"
+    ];
+  };
 in
 {
   nix = {
@@ -38,16 +45,7 @@ in
       options = "--delete-older-than 1w";
     };
 
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-
-      inherit extra-substituters;
-      inherit extra-trusted-public-keys;
-      inherit trusted-users;
-    };
+    inherit settings;
   };
 
   # Allow unfree packages
@@ -63,9 +61,10 @@ in
     '';
 
     "nix/nix.custom.conf".text = ''
-      extra-substituters = ${lib.concatStringsSep " " extra-substituters}
-      extra-trusted-public-keys = ${lib.concatStringsSep " " extra-trusted-public-keys}
-      trusted-users = ${lib.concatStringsSep " " trusted-users}
+      experimental-features = ${lib.concatStringsSep " " settings.experimental-features}
+      extra-substituters = ${lib.concatStringsSep " " settings.extra-substituters}
+      extra-trusted-public-keys = ${lib.concatStringsSep " " settings.extra-trusted-public-keys}
+      trusted-users = ${lib.concatStringsSep " " settings.trusted-users}
     '';
   };
 }
