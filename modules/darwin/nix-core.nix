fc456dd74b6efc73da02e79a2ba4b42a42e3f0d5
yxtay <5795122+yxtay@users.noreply.github.com>
2025-03-17 17:28:16 +0800

    fix: nix

--- modules/darwin/nix-core.nix
+++ modules/darwin/nix-core.nix
@@ -1,18 +1,36 @@
 inputs@{ pkgs, lib, ... }:
-{
-  imports = [
-    inputs.nix-index-database.darwinModules.nix-index
+let
+  extra-substituters = [
+    "https://nix-community.cachix.org"
+    "https://nixpkgs-unfree.cachix.org"
+    "https://cachix.cachix.org"
+    "https://devenv.cachix.org"
+    "https://cache.flox.dev"
+    "https://numtide.cachix.org"
   ];
-
+  extra-trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+    "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+  ];
+  trusted-users = [
+    "root"
+    "@admin"
+  ];
+in
+{
   nix = {
+    enable = false;
     package = lib.mkDefault inputs.nix.packages.${pkgs.system}.default;
 
     channel.enable = false;
-    optimise.automatic = true;
-    useDaemon = true;
+    optimise.automatic = false;
 
     gc = {
-      automatic = true;
+      automatic = false;
       interval = {
         Hour = 3;
         Minute = 15;
@@ -26,29 +44,28 @@ inputs@{ pkgs, lib, ... }:
         "flakes"
       ];
 
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-        "https://nixpkgs-unfree.cachix.org"
-        "https://cachix.cachix.org"
-        "https://devenv.cachix.org"
-        "https://cache.flox.dev"
-        "https://numtide.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      ];
-
-      trusted-users = [
-        "@admin"
-      ];
+      extra-substituters = extra-substituters;
+      extra-trusted-public-keys = extra-trusted-public-keys;
+      trusted-users = trusted-users;
     };
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+
+  environment.etc = {
+    "determinate/config.json".text = ''
+      {
+        "garbageCollector": {
+          "strategy": "automatic"
+        }
+      }
+    '';
+
+    "nix/nix.custom.conf".text = ''
+      extra-substituters = ${lib.concatStringsSep " " extra-substituters}
+      extra-trusted-public-keys = ${lib.concatStringsSep " " extra-trusted-public-keys}
+      trusted-users = ${lib.concatStringsSep " " trusted-users}
+    '';
+  };
 }
