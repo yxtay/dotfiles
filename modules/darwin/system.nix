e77a6187053cc7988d4577a8d64d24759bf89bec
YuXuan Tay <5795122+yxtay@users.noreply.github.com>
2025-06-06 10:39:40 +0800

    feat: install-rosetta (#226)

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -15,6 +15,11 @@
     primaryUser = user.name;
 
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+    activationScripts.extraActivation.text = ''
+      # install rosetta
+      softwareupdate --install-rosetta --agree-to-license
+    '';
+
     activationScripts.postActivation.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
