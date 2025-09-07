5cc8421ecd4db9b02c9ac7499e39f1793e9c5332
yxtay <5795122+yxtay@users.noreply.github.com>
2025-09-07 15:05:17 +0800

    Update chezmoi.nix

--- modules/home/chezmoi.nix
+++ modules/home/chezmoi.nix
@@ -8,15 +8,17 @@
   home.packages = [ pkgs.chezmoi ];
 
   home.activation.chezmoi = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [[ ! -v DRY_RUN ]]; then
-      chezmoi=${pkgs.chezmoi}/bin/chezmoi
-      source_path=$("$chezmoi" source-path)
-
-      args=(apply --force)
-      if [[ $source_path == */.local/share/chezmoi || $source_path == /nix/store/* ]]; then
-        args+=(--init --source "${self}")
-      fi
-      "$chezmoi" "''${args[@]}"
+    if [[ -v DRY_RUN ]]; then
+      exit;
     fi
+
+    chezmoi=${pkgs.chezmoi}/bin/chezmoi
+    source_path=$("$chezmoi" source-path)
+
+    args=(apply --force)
+    if [[ $source_path == */.local/share/chezmoi || $source_path == /nix/store/* ]]; then
+      args+=(--init --source "${self}")
+    fi
+    "$chezmoi" "''${args[@]}"
   '';
 }
