6cfbb5e9443b5e32555dcec316f8ef32f64b8951
yxtay <5795122+yxtay@users.noreply.github.com>
2025-10-28 13:59:12 +0800

    Update cleanup-all.sh

--- scripts/cleanup-all.sh
+++ scripts/cleanup-all.sh
@@ -18,5 +18,5 @@ fi
 if command -v npm >/dev/null; then npm cache clean --force; fi
 if command -v trunk >/dev/null; then trunk cache prune; fi
 
-find "${HOME}/.cache/huggingface/"{datasets/,xet/} -type f -mtime +7 -delete
+find "${HOME}/.cache/huggingface/" -not \( -path "${HOME}/.cache/huggingface/hub/*" \) -type f -mtime +7 -delete
 find "${HOME}/.cache/" -type d -empty -depth -delete
