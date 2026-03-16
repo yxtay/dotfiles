dd92ccda44a7e77ee7840aa97ffb78fa6003d7d1
yxtay <5795122+yxtay@users.noreply.github.com>
2026-03-16 14:41:49 +0800

    fix: remove-homebrew-patch

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,26 +1,26 @@
 inputs@{
   pkgs,
-  lib,
-  config,
+  # lib,
+  # config,
   user,
   ...
 }:
 {
   # https://github.com/zhaofengli/nix-homebrew/issues/3
-  system.activationScripts.extraActivation.text = lib.mkOrder 1501 (
-    lib.concatLines (
-      lib.mapAttrsToList (
-        prefix: d:
-        if d.enable then
-          ''
-            sudo chown -R ${config.nix-homebrew.user} ${prefix}/bin
-            sudo chgrp -R ${config.nix-homebrew.group} ${prefix}/bin
-          ''
-        else
-          ""
-      ) config.nix-homebrew.prefixes
-    )
-  );
+  # system.activationScripts.extraActivation.text = lib.mkOrder 1501 (
+  #   lib.concatLines (
+  #     lib.mapAttrsToList (
+  #       prefix: d:
+  #       if d.enable then
+  #         ''
+  #           sudo chown -R ${config.nix-homebrew.user} ${prefix}/bin
+  #           sudo chgrp -R ${config.nix-homebrew.group} ${prefix}/bin
+  #         ''
+  #       else
+  #         ""
+  #     ) config.nix-homebrew.prefixes
+  #   )
+  # );
 
   imports = [ inputs.nix-homebrew.darwinModules.nix-homebrew ];
 
